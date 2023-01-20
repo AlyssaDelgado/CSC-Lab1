a5971f5f0dd8ede7a1c759ef9db0277d81d84855
AlyssaDelgado <123202117+AlyssaDelgado@users.noreply.github.com>
2023-01-20 15:21:36 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "--------------------------------" << endl;
+  cout<<"Alyssa Delgado"<<endl;
+  cout<< "Computer Science 1"<<endl;
+  cout<<"Lab 1"<<endl;
+  cout<<"January 20, 2023"<<endl;
+  cout << "--------------------------------" << endl;
+  int x; // integer variable
+  int y; // integer variable;
+  x = 20;
+  //cout<<"content of X right after initialization" << x << endl;
+  y = 5;
+  cout<<"x + Y = " << x + y<<endl;
+  //cout<<"content of y right before program exit: " << y << endl;
+
+  return 0;
+}
